bab003adc6390950b6f759b7c599166721b6d4c6
Nexhade <henriquewilian.crs@gmail.com>
2026-01-13 21:53:10 -0300

    Exercicio de como calcular notas, utilizei váriaveis em float para as notas e a média final mostrei o valor inteiro facilitando para o professor analisar o aluno

---
+++ Mundo 1/Exercício 7.py
@@ -0,0 +1,13 @@
+#Deselvolva um programa que leia duas notas de um aluno
+#Calcule e mostre a sua média
+
+from os import system
+
+system('clear')
+
+print('Sitema para calucar a média de um aluno')
+
+nota1 = float(input('\nDigite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+print(f'\nA média das duas notas é {(nota1+nota2)/2}')
